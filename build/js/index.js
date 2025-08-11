6ff6e4b23189c604c68c5f94dc970d5f87a0a033
Coconut8201 <cocom714kv@gmail.com>
2025-08-11 16:39:36 +0800

    套用新的 .gitignore 規則

--- build/js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let btn = document.querySelector("#userlogin");
-let userlogin = document.querySelector("#userloginDialog");
-let closeDialog = document.querySelector("#close");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
-    userlogin === null || userlogin === void 0 ? void 0 : userlogin.open();
-});
